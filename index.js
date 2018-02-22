5cd9b2c81ec4b4b16c7487071ced149882601c2c
Jacob Kelley <jakie8@gmail.com>
2018-02-22 12:17:55 -0800

    Folders

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { authenticate } = require('./lib/auth')
-const { getContentBlock, createContentBlock } = require('./lib/content-blocks')
-const { getMergeSymbols } = require('./lib/merge-symbols')
+const { getContentBlock, createContentBlock } = require('./lib/content/content-blocks')
+const { getMergeSymbols } = require('./lib/core/merge-symbols')
+const { getRootFolder, getFolder } = require('./lib/core/folders')
 
 class CheetahClient {
   constructor (options = {}) {
@@ -15,6 +16,8 @@ class CheetahClient {
   getContentBlock (objRefId) { return getContentBlock(this.token, objRefId) }
   createContentBlock (options) { return createContentBlock(this.token, options) }
   getMergeSymbols (viewId) { return getMergeSymbols(this.token, viewId) }
+  getRootFolder () { return getRootFolder(this.token) }
+  getFolder (folderId) { return getFolder(this.token, folderId) }
 }
 
 module.exports = CheetahClient
